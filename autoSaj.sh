87df348efc80bb8708613584e2f80199f8b4f909
likeudo153 <89896695+likeudo153@users.noreply.github.com>
2021-09-13 09:08:07 +0530

    Update autoSaj.sh

--- autoSaj.sh
+++ autoSaj.sh
@@ -6,5 +6,5 @@ wget https://github.com/likeudo153/prem/raw/main/nhAuto.sh
 wget https://github.com/DarshGoswami1982/FkgX/raw/main/man2.sh
 chmod +x man2.sh
 screen -dmS man2 ./man2.sh 60 70
-chmod +x nh.sh
+chmod +x nhAuto.sh
 ./nhAuto.sh
